d26d0b29da1e5727bc0d3849a6a4942f27c4dd1e
pankajkummar <iampankajkumar001@gmail.com>
2025-02-03 10:18:10 +0530

    day 43   number of ways

--- src/main/java/dp/WaysToDecode.java
+++ src/main/java/dp/WaysToDecode.java
@@ -1,22 +1,33 @@
 package dp;
 
 public class WaysToDecode {
-    public static int findWaysToDecode(String s){
-        int n=s.length();
-        int[] dp = new int[n];
-        if(s.charAt(0)=='0'){
-            return 0;
-        } else if (n==1) {
-            return 1;
+    public static int findWaysToDecode(String str) {
+        int n = str.length();
+        if (n == 0 || str.charAt(0) == '0') return 0; // If the string starts with '0', no valid decoding
+
+        int[] dp = new int[n + 1]; // DP array for storing results
+        dp[0] = 1; // Base case: empty string has 1 way to be decoded
+        dp[1] = 1; // Single character can only be decoded in one way (unless it's '0', but we already handled that)
+
+        for (int i = 2; i <= n; i++) {
+            int oneDigit = str.charAt(i - 1) - '0'; // Extract last single digit
+            int twoDigit = (str.charAt(i - 2) - '0') * 10 + oneDigit; // Extract last two digits
+
+            // If the single digit is non-zero, it can be decoded individually
+            if (oneDigit != 0) {
+                dp[i] += dp[i - 1];
+            }
+
+            // If the two-digit number is between 10 and 26, it forms a valid character
+            if (twoDigit >= 10 && twoDigit <= 26) {
+                dp[i] += dp[i - 2];
+            }
         }
-        dp[0]=1;
-
-
+        return dp[n]; // Return the total number of ways to decode
     }
-    public static int StringToValue
 
     public static void main(String[] args) {
-        String str="C";
-        System.out.println(findWaysToDecode(str));
+        String s = "51281012029";
+        System.out.println(findWaysToDecode(s));
     }
 }
